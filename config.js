1c12d467a742ec83deb6d4b1e126cc2dfe476027
gturriza <65683184+gturriza@users.noreply.github.com>
2020-06-17 00:44:21 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/erjercicio";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/audioaprende";
 exports.PORT = process.env.PORT || 8080;
